04ec360d69be419f6774656c1d4e8e6b0bb4d08c
mizchi <miz404@gmail.com>
2017-02-20 19:53:29 +0900

    Fix for static site

--- application/src/contexts/zoom-subscriber.ts
+++ application/src/contexts/zoom-subscriber.ts
@@ -1,23 +1,25 @@
 var zoomRate = 1.0;
 const g: any = global;
 var subscriber = (context, subscribe) => {
-  var webFrame = g.require('electron').webFrame;
-  subscribe('zoom:plus', () => {
-    if (zoomRate >= 2.5) {
-      return;
-    }
-    zoomRate += 0.1;
-    webFrame.setZoomFactor(zoomRate);
-  });
+  if (global.global) {
+    var webFrame = g.require('electron').webFrame;
+    subscribe('zoom:plus', () => {
+      if (zoomRate >= 2.5) {
+        return;
+      }
+      zoomRate += 0.1;
+      webFrame.setZoomFactor(zoomRate);
+    });
 
-  subscribe('zoom:minus', () => {
-    // Inhibit too much small
-    if (zoomRate <= 0.4) {
-      return;
-    }
-    zoomRate -= 0.1;
-    webFrame.setZoomFactor(zoomRate);
-  });
+    subscribe('zoom:minus', () => {
+      // Inhibit too much small
+      if (zoomRate <= 0.4) {
+        return;
+      }
+      zoomRate -= 0.1;
+      webFrame.setZoomFactor(zoomRate);
+    });
+  }
 };
 
 export = subscriber;
